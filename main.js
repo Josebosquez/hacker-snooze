711cdecd514112e10db6fd09edbb0ab28b30ecfa
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-04-13 17:39:17 -0600

    compeleted

--- main.js
+++ main.js
@@ -1,59 +1,73 @@
-// query for container two
-const containerTwo = document.querySelector('.container-2');
+//================
+//news app
+//================
 
-//right side of middle container
-const container3 = document.createElement('div');
-container3.className = 'container-3';
-
-//the div containing the articles
-const articleBox = document.createElement('div');
-articleBox.className = "articleBox"
-
-//title of the article box
-const articleBoxTitle = document.createElement('div')
-
-articleBoxTitle.className = "articleBoxTitle";
-
-// main sub components div
-const mainSub = document.createElement("div");
-mainSub.className = "MainSub"
-
-// score
-const score = document.createElement("div");
-score.className="articleBoxScore";
-
-// author
-const author = document.createElement('div');
-author.className = "articleBoxAuthor";
-
-//comments
-const comments = document.createElement('div');
-comments.className = "articleBoxComments"
-
-
-
-
-containerTwo.appendChild(container3);
-container3.appendChild(articleBox);
-articleBox.appendChild(articleBoxTitle)
-articleBox.appendChild(mainSub)
-mainSub.appendChild(score)
-mainSub.appendChild(author)
-mainSub.appendChild(comments)
-
-articleBoxTitle.innerHTML="title"
-score.innerHTML="score";
-author.innerHTML="author";
-comments.innerHTML="comments";
+// one function to store everything
+function reportData() {
+    //get the initial api pull
+    function initialPull() {
+        // create url variable for 1st api
+        const topStoriesAPI = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
+        //fetch first api
+        fetch(topStoriesAPI)
+        //first then statement grabs json info from api
+            .then(response => response.json())
+            // second then statement grabs json info and makes an arr from 0-99
+            .then(function (arr) {
+                arr = arr.slice(0, 99);
+                // loop through arr to find the number from the json info above and insert it into the story api below.
+                arr.forEach(function (num) {
+                    const storyAPI = `https://hacker-news.firebaseio.com/v0/item/${num}.json?print=pretty`
+                    // call function thats below to get second api call
+                    getStoryAPI(storyAPI)
+                });
+            })
+    }
+    // second api call
+    function getStoryAPI(storyAPI) {
+        //fetch second api that was made into a variable above.
+        fetch(storyAPI)
+        //first .then statement grabs storyAPI json.
+            .then(response => response.json())
+            // second then statement grabs storyAPI json info.
+            .then(function (storyAPI) {
+                // use created function below to make create html
+                createHTML(storyAPI)
+            })
+    }
 
 
-// const container3 = `<div class="container-3">
-//                         <div class="articleBox">
-//                             <div id="articleBoxTitle">title</div>
-//                             <div class="MainSub">
-//                                 <div id="articleBoxScore">score</div>
-//                                 <div id="articleBoxAuthor">author</div>
-//                                 <div id="articleBoxComments">comments</div>
-//                             </div>
-//                         </div>
-//                     </div>`
+    // create html divs for dom use
+
+    // query container to get container div 
+    const container2 = document.querySelector('.container-2')
+    // create function to allow me to call it above
+    function createHTML(storyAPI) {
+        //create variable for divs, use template string to request info for title, score, author, and comments
+        let html = `
+        <div class="container-3">
+            <div class="articleBox">
+                <div id="articleBoxTitle"><a class = "link" target = "_blank" href="${storyAPI.url}"> ${storyAPI.title}</a> </div>
+                <br>
+                <div class="MainSub">
+                    <div id="articleBoxScore">Score: ${storyAPI.score}</div>
+                    <div id="articleBoxAuthor">Author: ${storyAPI.by}</div>
+                    <div id="articleBoxComments">Comments: ${storyAPI.kids.length}</div>
+                </div>
+            </div>
+        </div>`
+        // add our html div to the container (just like appending)
+        container2.innerHTML += html;
+    }
+    // call the function again incase there is a number left in our arr
+    initialPull()
+    const button = document.querySelector("#button");
+    const tick = new Audio('GotchaBitch!!!.mp3');
+
+    button.addEventListener("click", function() {
+    tick.play ();
+    })
+}
+
+// call the function to make everything work
+reportData()
